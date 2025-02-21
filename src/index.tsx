c4bb486b2bf2c290e0ef3acb83d5a5d791d74e34
Owen Moogk <owenmoogk@gmail.com>
2025-02-21 17:23:27 -0500

    Migrate to vite

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
+  </StrictMode>
 );
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    eslint({
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      emitWarning: true,
+      emitError: false, // error doesn't block compilation
+      failOnWarning: false,
+      failOnError: false, // error doesn't block compilation
+    }),
+  ],
+  server: {
+    port: 3000,
+  },
+//   resolve: {
+//     alias: {
+//       '@components': path.resolve(__dirname, './src/components'),
+//       '@api': path.resolve(__dirname, './src/api'),
+//       '@global': path.resolve(__dirname, './src/global'),
+//     },
+//   },
+});
